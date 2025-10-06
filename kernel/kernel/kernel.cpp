2f64fc832f52fa4c60216437eb3fd8be4d6f4169
Artjoms <artv20022@gmail.com>
2025-10-06 23:35:54 +0300

    feat: add GDT

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <kernel/tty.h>
+#include <kernel/gdt.hpp>	
 
 extern "C" void kernel_main(void)
 {
+	GlobalDescriptorTable gdt;
 	terminal_initialize();
 	printf("Hello, MYOS C++ kernel!!!\n");
 }
