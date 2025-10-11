6772ec736ee5e8d1214e699dd17e37616922e3a0
Artjoms <artv20022@gmail.com>
2025-10-11 14:23:44 +0300

    feat: add interrupts support

--- kernel/kernel/kernel.cpp
+++ kernel/kernel/kernel.cpp
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <kernel/tty.h>
-#include <kernel/gdt.hpp>	
-
+#include <kernel/gdt.hpp>
+#include <kernel/interrupts.hpp>
 extern "C" void kernel_main(void)
 {
-	GlobalDescriptorTable gdt;
 	terminal_initialize();
 	printf("Hello, MYOS C++ kernel!!!\n");
+
+	GlobalDescriptorTable gdt;
+	InterruptManager interrupts(0x20, &gdt);
+
+	interrupts.Activate();
+
+	while (1)
+		;
 }
