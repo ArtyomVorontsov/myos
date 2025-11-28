e42bdbc6f896e314b03af1f9c2d58236672ec23b
Artjoms <artv20022@gmail.com>
2025-11-28 22:56:32 +0200

    feat: add to qemu AMD am79c973 network card simulation

--- qemu.sh
+++ qemu.sh
@@ -3,4 +3,4 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso -d int,cpu_reset -D qemu.log -monitor stdio -machine pc -m 128M
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom myos.iso -d int,cpu_reset -D qemu.log -monitor stdio -machine pc -m 128M -net nic,model=pcnet
